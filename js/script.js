bf7749b0a58ebe5b84d965a14b9071ac1a6f91c9
Vic-NG <61920659+Vic-NG@users.noreply.github.com>
2021-03-25 18:15:49 -0400

    função de verificar numeros refeita

--- js/script.js
+++ js/script.js
@@ -1,33 +1,25 @@
 let numeroSecreto = parseInt(Math.random() * 10)
-let botao = document.querySelector('button');
+let botao = document.querySelector("button");
+let chances = 3;
 
+function verificaNumero() {
+let chute = document.querySelector("input").value;
 
-
-function verificaNumero()
-{
-    let chances = 3; 
-    
-
-    while(chances > 0)
-    {
-            let chute = document.querySelector('input').value;
-            if(numeroSecreto == chute){
-                alert('Acertou miseravi!')
-                break
-            }
-
-            else if(chute > numeroSecreto){
-               alert('Errou. O numero é menor')
-                chances = chances - 1
-                
-            }
-            
-            else if(chute < numeroSecreto){
-               alert('Errou. O numero é maior')
-                chances = chances - 1
-            }
-    }
+  if (numeroSecreto == chute) {
+    alert("Acertou miseravi!");
+    return;
+  } else if (chute > numeroSecreto) {
+    alert("Errou. O numero é menor");
+    chances = chances - 1;
+  } else {
+    alert("Errou. O numero é maior"); 
+    chances = chances - 1;
+  } 
+  
+  if(chances == 0) {
+    alert("Suas chances acabaram ):"); 
+    location.reload();
+  }
+  console.log(chances);
+  return;
 }
-
-botao.onclick = verificaNumero;
-
